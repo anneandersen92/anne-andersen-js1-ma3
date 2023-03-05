29a269433afd57c9f6e6f0ec46e6e9b72da27c7e
anneandersen92 <anne_92@live.no>
2023-03-05 21:58:50 +0100

    complete

--- js/question-2.js
+++ js/question-2.js
@@ -4,29 +4,33 @@ const url = `https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=
 const resultsContainer = document.querySelector(".results");
 const loader = document.querySelector(".loader");
 
+let html = "";
+
 async function getGames() {
+    try {
+        const response = await fetch(url);
+        const jsonResults = await response.json();
+        const results = jsonResults.results;
 
-    const response = await fetch(url);
-    const jsonResults = await response.json();
-    const results = jsonResults.results;
+        for (let i = 0; i < results.length; i++) {
+            html += `<div class="games">
+                        <h2 class="name">${results[i].name}</h2>
+                        <p class="rating">Rating: ${results[i].rating} / 5</p>
+                        <p class="tags">Tags: ${results[i].tags.length}</p>
+                    </div>`
 
-    console.log(results)
-
-    html = "";
-
-    for (let i = 0; i < results.length; i++) {
-        html += `<div class="games">
-                    <h3 class="name">🎮 ${results[i].name}<\h3>
-                    <p class="rating">⭐ ${results[i].rating}/5<\p>
-                    <p class="tags">🏷️ ${results[i].tags.length}<\p>
-                <\div>`
-
-        if (i===7){
-            break;
+            if (i===7){
+                break;
+            }
         }
+    } catch(error) {
+        html += `<div class="error">
+                <h2>An error occured!<\h2>
+                <p>${error}</p>
+                <\div>`
+    } finally {
+        resultsContainer.innerHTML = html;
     }
-
-    resultsContainer.innerHTML = html;
 }
 
 getGames();
